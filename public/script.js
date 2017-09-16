a6e2090be32653914e2d049f6b2aad44b78fdf46
Carlos Matheu Armengol <info@plastikaweb.com>
2017-09-16 11:31:20 +0200

    final touches

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 var PRICE = 9.99;
+var PAG = 10;
 
 new Vue({
   el: '#app',
@@ -6,23 +7,41 @@ new Vue({
     total: 0,
     items: [],
     cart: [],
+    results: [],
     search: 'anime',
     lastSearch: '',
     loading: false,
     price: PRICE
   },
+  computed: {
+    noMoreItens: function() {
+      return        this.items.length === this.results.length && this.results.length > 0;
+    }
+  },
   methods: {
+    appendItems: function() {
+      if (this.items.length < this.results.length) {
+        var append = this.results.slice(
+          this.items.length,
+          this.items.length + PAG
+        );
+        this.items = this.items.concat(append);
+      }
+    },
     onSubmit: function() {
-      this.items = [];
-      this.loading = true;
-      this.$http.get('/search/'.concat(this.search)).then(
-        function(res) {
-          this.lastSearch = this.search;
-          this.items = res.data;
-          this.loading = false;
-        },
-        function(err) {}
-      );
+      if (this.search.length) {
+        this.items = [];
+        this.loading = true;
+        this.$http.get('/search/'.concat(this.search)).then(
+          function(res) {
+            this.lastSearch = this.search;
+            this.results = res.data;
+            this.appendItems();
+            this.loading = false;
+          },
+          function(err) {}
+        );
+      }
     },
     addItem: function(index) {
       var item = this.items[index];
@@ -67,6 +86,12 @@ new Vue({
     }
   },
   mounted: function() {
+    var vue = this;
+    var elem = document.getElementById('product-list-bottom');
+    var watcher = scrollMonitor.create(elem);
+    watcher.enterViewport(function() {
+      vue.appendItems();
+    });
     this.onSubmit();
   }
 });
